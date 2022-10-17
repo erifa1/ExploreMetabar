34b2390bb2d5cbbe039edaad67aadb3d0ab2150e
Sebastien Theil <sebastien.theil@inra.fr>
2022-10-17 08:53:52 +0200

    Changed r$sdat() format.

--- R/mod_cluster.R
+++ R/mod_cluster.R
@@ -78,26 +78,29 @@ mod_cluster_ui <- function(id){
 mod_cluster_server <- function(input, output, session, r = r){
 
     ns <- session$ns
-    
+
     observe({
+      req(r$phyloseq_filtered_norm())
       updateSelectInput(session, "clstr_rank_glom",
-                        choices = c( rank_names(r$phyloseq_filtered_norm()), "ASV" ),
+                        choices = c( phyloseq::rank_names(r$phyloseq_filtered_norm()), "ASV" ),
                         selected = "ASV")
     })
-    
+
     observe({
       req(r$phyloseq_filtered_norm())
       updateSelectInput(session, "clust_fact1",
-                        choices = r$phyloseq_filtered_norm()@sam_data@names)
+                        choices = colnames(r$sdat()))
     })
-    
+
     observe({
-      updateNumericRangeInput(session, 'clstr_minAb',"Minimum taxa overall raw abundance:", value=c(0,max(taxa_sums(r$phyloseq_filtered_norm()))))
+      req(r$phyloseq_filtered_norm())
+      updateNumericRangeInput(session, 'clstr_minAb',
+                              "Minimum taxa overall raw abundance:", value=c(0,max(taxa_sums(r$phyloseq_filtered_norm()))))
     })
-    
+
     observe({
-      req(r$phyloseq_filtered())
-      if(is.null(phy_tree(r$phyloseq_filtered(), errorIfNULL=FALSE))){
+      req(r$phyloseq_filtered_norm())
+      if(is.null(phy_tree(r$phyloseq_filtered_norm(), errorIfNULL=FALSE))){
         ch1 = list("bray", "jaccard")
       }else{
         ch1 = list("bray", "jaccard", "unifrac", "wunifrac")
@@ -106,18 +109,21 @@ mod_cluster_server <- function(input, output, session, r = r){
                          choices = ch1, inline = TRUE)
     })
 
-    
+
     compute.dist <- reactive({
+      req(r$phyloseq_filtered_norm())
       dd <- phyloseq::distance(r$phyloseq_filtered_norm(), method = input$dist.meth, type="sample")
       return(dd)
     })
-    
+
     compute.clust <- reactive({
+      req(compute.dist())
       hc <- hclust(compute.dist(), method = input$hclust.meth)
       return(hc)
     })
-    
+
     compute.k <- eventReactive(input$launch_clust, {
+      req(r$phyloseq_filtered_norm(), compute.dist(), compute.clust())
       if(input$k.meth == "silhouette"){
         Si <- numeric(nrow(t(otu_table(r$phyloseq_filtered_norm()))))
         for (k in 2:(nrow(t(otu_table(r$phyloseq_filtered_norm()))) -1)){
@@ -139,17 +145,19 @@ mod_cluster_server <- function(input, output, session, r = r){
       }
       return(k.best)
     })
-    
+
     compute.cutree <- reactive({
+      req(compute.clust(), compute.k())
       ct <- cutree(compute.clust(), k=compute.k())
       ct <- as.data.frame(ct)
       colnames(ct) <- c('clstr')
       ct$fact <- r$sdat()[rownames(ct),input$clust_fact1]
       return(ct)
     })
-    
+
     plot.dendro <- eventReactive(input$launch_clust,{
       dd <- as.dendrogram(compute.clust())
+      # browser()
       colours <- colourvalues::colour_values(r$sdat()[labels(dd),input$clust_fact1], palette="viridis")
       dd <- dendextend::color_labels(dd, col=colours)
       if(input$branchcolor){
@@ -166,17 +174,17 @@ mod_cluster_server <- function(input, output, session, r = r){
       } else{
         plot(dd, leaflab="none", horiz = TRUE)
       }
-        
-      
+
+
     })
-    
-    
+
+
     output$dendro.plot <- renderPlot({
       plot.dendro()
     })
-    
-    
-    
+
+
+
     plot.sample.by.clstr <- eventReactive(input$launch_clust, {
       ct <- compute.cutree()
       ct %>%
@@ -186,21 +194,21 @@ mod_cluster_server <- function(input, output, session, r = r){
         geom_bar(stat="identity") +
         facet_grid(. ~ clstr) +
         theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
-      
+
     })
-    
+
     output$sample.by.clstr <- renderPlot({
       plot.sample.by.clstr()
     })
-    
-    
+
+
     plot.subtree <- reactive({
       k <- compute.k()
       dend <- compute.clust()
       dend_list <- get_subdendrograms(as.dendrogram(dend), k, order_clusters_as_data = TRUE)
       dd <- dend_list[[as.numeric(input$clust_nb)]]
       sub.phy <- phyloseq::prune_samples(labels(dd), r$phyloseq_filtered_norm())
-      
+
       # taxa glom
       if(input$clstr_rank_glom != 'ASV'){
         tmp <- fast_tax_glom(sub.phy, input$clstr_rank_glom)
@@ -209,43 +217,43 @@ mod_cluster_server <- function(input, output, session, r = r){
         taxa_names(tmp) <- nnames
         sub.phy <- tmp
       }
-      
-      
+
+
       sub.phy <- phyloseq::prune_taxa(phyloseq::taxa_sums(sub.phy)>input$clstr_minAb, sub.phy)
       order.dendrogram(dd) <- as.integer(rank(order.dendrogram(dd)))
-      
+
       otable <- phyloseq::otu_table(sub.phy)
       data.com <- reshape2::melt(otable)
-      
-      data.com$xlabel <- as.factor(r$sdat()[as.character(data.com$Var2),input$clust_fact1])
+      #browser()
+      data.com$xlabel <- as.factor(as(r$sdat(), "data.frame")[as.character(data.com$Var2),input$clust_fact1])
       names(data.com) <- c("Tax", "Sample", "Abundance", "xlabel")
       # data.com$Tax = factor(data.com$Tax, levels = sort(unique(as.character(data.com$Tax))))
-      
+
       p.heat <- ggplot(data.com, aes(x = Sample, y = Tax)) + geom_tile(aes(fill = Abundance))
       p.heat <- p.heat + scale_fill_distiller("Normalized abundance", palette = "RdYlBu") + theme_bw()
-      
+
       # Make bacterial names italics
       p.heat <- p.heat + theme(axis.text.y = element_text(colour = 'black',
                                                           size = 10,
                                                           face = 'italic'))
       # Make seperate samples based on main varaible
       p.heat <- p.heat + facet_grid(~xlabel, scales = "free")
-      
+
       p.heat <- p.heat + ylab("Taxa")
-      
+
       #Clean the x-axis
       p.heat <- p.heat + theme(axis.title.x=element_blank(),
                                axis.text.x=element_text(angle = 90),
                                axis.ticks.x=element_blank())
-      
+
       # Clean the facet label box
       p.heat <- p.heat + theme(legend.key = element_blank(),
                                strip.background = element_rect(colour="black", fill="white"))
       cat(file=stderr(), 'done', "\n")
       return(p.heat)
     })
-    
-    
+
+
     compute.indicSpe <- reactive({
       k <- compute.k()
       dend <- compute.clust()
@@ -255,31 +263,31 @@ mod_cluster_server <- function(input, output, session, r = r){
       indval <- indicspecies::multipatt(otable, grp, control = permute::how(nperm = 99), duleg = TRUE)
       summary(indval)
     })
-    
-    
+
+
     output$indicSpe <- renderPrint({
       req(compute.k(), compute.clust(), compute.cutree())
       compute.indicSpe()
     })
-    
-    
+
+
     output$subclstr_plot <- renderPlot({
       p <- plot.subtree()
       p
     })
-    
-  
+
+
     output$nb_clstr <- renderPrint({
       req(compute.k())
       k <- compute.k()
       cat('Number of clusters: ', k)
     })
-    
+
     observe({
       updateSelectInput(inputId = "clust_nb",
                         choices = 1:compute.k())
     })
-    
+
   
     
 }
