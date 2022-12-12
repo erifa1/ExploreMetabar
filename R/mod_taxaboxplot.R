c84f5b57e6caca5a607af6f64dfa468ddc7614d1
Sebastien Theil <sebastien.theil@inra.fr>
2022-12-12 11:16:06 +0100

    add pickerInput and multiple select.

--- R/mod_taxaboxplot.R
+++ R/mod_taxaboxplot.R
@@ -25,10 +25,11 @@ mod_taxaboxplot_ui <- function(id){
               icon = icon("info-circle"), fill=TRUE, width = 10),
 
       box(
-        selectInput(
+        shinyWidgets::pickerInput(
           ns("boxplot_fact1"),
           label = "Select factor to test: ",
-          choices = ""
+          choices = "",
+          multiple = TRUE
         ),
         uiOutput(ns('ui_radio_tests')),
         actionButton(ns("go1"), "Run Test/Correlation", icon = icon("play-circle"),
@@ -69,20 +70,58 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
 
   observe({
     req(r$phyloseq_filtered(), r$var_list())
-    updateSelectInput(session, "boxplot_fact1",
-                      choices = r$var_list())
+    shinyWidgets::updatePickerInput(session, "boxplot_fact1",
+                      choices = r$var_list(),
+                      selected = r$var_list()[2])
 
   })
   
   
   isNumFactor <- reactive({
-    req(input$boxplot_fact1, r$sdat())
-    metadata <- r$sdat()
-    if(is.numeric(metadata[, input$boxplot_fact1])){
+    req(get_meta_col(), local_metadata())
+    metadata <- local_metadata()
+    if(is.numeric(metadata[, get_meta_col()])){
       return(TRUE)
     } else{
       return(FALSE)
     }
+  })
+  
+  
+  get_meta_col <- reactive({
+    req(input$boxplot_fact1, r$sdat())
+    metadata <- r$sdat()
+    if(length(input$boxplot_fact1) == 1){
+      meta.col <- input$boxplot_fact1
+    } else if(length(input$boxplot_fact1) > 1) {
+      validate(
+        need(!any(sapply(metadata[, input$boxplot_fact1], is.numeric)), message = "You can't select multiple with numeric factors")
+      )
+      meta.col <- paste0(input$boxplot_fact1, collapse='_')
+    }
+    return(meta.col)
+  })
+  
+  
+  local_metadata <- reactive({
+    req(input$boxplot_fact1, r$sdat())
+    metadata <- r$sdat()
+    if(! all(sapply(metadata[, input$boxplot_fact1], is.numeric))){
+      metadata <- tidyr::unite(metadata, !!get_meta_col(), input$boxplot_fact1, na.rm=TRUE)
+      metadata[, get_meta_col()] <- as.factor(metadata[, get_meta_col()])
+      metadata <- select(metadata, "sample.id", get_meta_col())
+    }
+    else{
+      metadata <- select(metadata, "sample.id", input$boxplot_fact1)
+    }
+    return(metadata)
+  })
+  
+  
+  local_physeq <- reactive({
+    phy <- r$phyloseq_filtered_norm()
+    sample_data(phy) <- sample_data(local_metadata())
+    return(phy)
   })
   
   
@@ -108,19 +147,22 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
 
   get_pval_table <- eventReactive(input$go1, {
     if(isNumFactor()){
-      get_corr_pval_table()
+      res <- get_corr_pval_table()
     } else{
-      get_kruskal_pval_table()
+      res <- get_kruskal_pval_table()
     }
+    t_table <- as.data.frame(tax_table(r$phyloseq_filtered_norm())) %>% rownames_to_column()
+    res <- left_join(res, t_table, by = c('taxa' = 'rowname'))
+    return(res)
   })
   
   
   get_merged_table <- reactive({
-    otable <- otu_table(r$phyloseq_filtered_norm()) %>% t() %>%
+    otable <- otu_table(local_physeq()) %>% t() %>%
       as.data.frame(stringsAsFactors = FALSE) %>%
       rownames_to_column('sample.id')
-    metadata <- r$sdat()
-    metadata <- metadata[, input$boxplot_fact1, drop=FALSE] %>% rownames_to_column('sample.id')
+    metadata <- local_metadata()
+    metadata <- metadata[, get_meta_col(), drop=FALSE] %>% rownames_to_column('sample.id')
     mtable <- left_join(otable, metadata, by='sample.id')
     return(mtable)
   })
@@ -130,8 +172,8 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
     mtable <- get_merged_table()
     results <- tibble('taxa' = as.character(),
                       'p.value' = as.numeric())
-    for(taxa in taxa_names(r$phyloseq_filtered_norm())){
-      res = kruskal.test(mtable[,taxa], mtable[,input$boxplot_fact1])
+    for(taxa in taxa_names(local_physeq())){
+      res = kruskal.test(mtable[,taxa], mtable[,get_meta_col()])
       results <- results %>% add_row('taxa' = taxa, 'p.value' = res$p.value)
     }
     return(results)
@@ -143,8 +185,8 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
     results <- tibble('taxa' = as.character(),
                       'p.value' = as.numeric(),
                       'cor.coef' = as.numeric())
-    for(taxa in taxa_names(r$phyloseq_filtered_norm())){
-      res <- stats::cor.test(mtable[,taxa], mtable[,input$boxplot_fact1], method = input$cor_test)
+    for(taxa in taxa_names(local_physeq())){
+      res <- stats::cor.test(mtable[,taxa], mtable[,get_meta_col()], method = input$cor_test)
       results <- results %>% add_row('taxa' = taxa, 'p.value' = res$p.value, cor.coef = res$estimate)
     }
     return(results)
@@ -162,7 +204,7 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
   ordertable1 <- reactive({
     mtable <- get_merged_table()
     if(input$order1){
-      fun = glue::glue( "mtable${input$boxplot_fact1} = factor( mtable${input$boxplot_fact1}, levels = gtools::mixedsort(unique(mtable${input$boxplot_fact1})) ) " )
+      fun = glue::glue( "mtable${get_meta_col()} = factor( mtable${get_meta_col()}, levels = gtools::mixedsort(unique(mtable${get_meta_col()})) ) " )
       eval(parse(text=fun))
     }
     return(mtable)
@@ -176,15 +218,15 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
       mtable <- get_merged_table()
       ptype <- 'scatter'
       select1  <- get_corr_pval_table()[input$pvalout1_row_last_clicked,'taxa'] %>% pull
-      p <- ggplotly(ggplot2::ggplot(data = mtable, aes_string(x = formulaic::add.backtick(select1), y = input$boxplot_fact1)) + 
+      p <- ggplotly(ggplot2::ggplot(data = mtable, aes_string(x = formulaic::add.backtick(select1), y = get_meta_col())) + 
                  geom_point() + 
                  geom_smooth(method = 'lm', se = T, na.rm = T, show.legend = T))
                    
     } else{
       mtable <- ordertable1()
       select1  <- get_kruskal_pval_table()[input$pvalout1_row_last_clicked,'taxa'] %>% pull
-      p <- plot_ly(mtable, x = as.formula(glue("~ {input$boxplot_fact1}")), y = as.formula(paste0("~", formulaic::add.backtick(select1))),
-                   color = as.formula(glue("~{input$boxplot_fact1}")), type = 'box')
+      p <- plot_ly(mtable, x = as.formula(glue("~ {get_meta_col()}")), y = as.formula(paste0("~", formulaic::add.backtick(select1))),
+                   color = as.formula(glue("~{get_meta_col()}")), type = 'box')
     }
     
     return(p)
@@ -195,7 +237,7 @@ mod_taxaboxplot_server <- function(input, output, session, r = r){
     if(is.null(input$pvalout1_row_last_clicked)){return(NULL)}
     mtable <- get_merged_table()
     select1  <- get_kruskal_pval_table()[input$pvalout1_row_last_clicked,'taxa'] %>% pull
-    res = pairwise.wilcox.test(mtable[,select1], mtable[,input$boxplot_fact1], p.adjust.method = 'none')
+    res = pairwise.wilcox.test(mtable[,select1], mtable[,get_meta_col()], p.adjust.method = 'none')
     return(res)
   })
 
