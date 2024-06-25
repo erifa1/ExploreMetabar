ec2b56431e9c636315a2bcf02c428629a41d5fd1
Sebastien Theil <sebastien.theil@inrae.fr>
2024-06-25 12:14:00 +0200

    Change version number

--- R/app_ui.R
+++ R/app_ui.R
@@ -16,7 +16,7 @@ app_ui <- function() {
     # List the first level UI elements here
     dashboardPage(skin = "red",
                   dashboardHeader(
-                      title = "Explore Metabar 2.0.2",
+                      title = "Explore Metabar 2.1.0",
 
                       tags$li(class="dropdown",tags$a("Hosted by", img(src = SK8img,
                       title = "SK8", height = "20px"), headerText = "Source code",href="https://sk8.inrae.fr/", target="_blank")),
